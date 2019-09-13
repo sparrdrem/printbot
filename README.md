0d551be2411a63aef31b3f79a1f2c6b59603f0e9
Innovation Inc <connorrox60@gmail.com>
2019-09-12 20:08:37 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# printbot
+Printbot - A bot made to list downloadable 3D Models for Discord
--- printbot.js
+++ printbot.js
@@ -1,55 +1,31 @@
+// Get dependencies and set up some variables
 const Discord = require('discord.js');
 const client = new Discord.Client();
-// Embed Menu coding
-const HelpMenuEmbed = new Discord.RichEmbed()
-  .setColor('777575') // Sets border color to a nice gray color
-  .setTitle('Printbot available commands')
-  .setAuthor('Printbot', 'https://raw.githubusercontent.com/sparrdrem/printbot/master/_previmg.png')
-  .setThumbnail('https://raw.githubusercontent.com/sparrdrem/printbot/master/_previmg.png')
-  .addField('Commands:', '_ _')
-  .addBlankField()
-  .addField('!help', 'show this menu')
-  .addField('!ver', 'list current version')
-  .addField('!listcat', 'list all categories \(use `!help listcat` for more detailed view\)')
-  .addField('!info', 'get the link to a 3D model, as well as info about it \(use `!help info` for more detailed view\)')
-
-const HelpMenuListcatEmbed = new Discord.RichEmbed()
-  .setColor('777575')
-  .setTitle('!listcat Command Information')
-  .setAuthor('Printbot', 'https://raw.githubusercontent.com/sparrdrem/printbot/master/_previmg.png')
-  .setThumbnail('https://raw.githubusercontent.com/sparrdrem/printbot/master/_previmg.png')
-  .addBlankField()
-  .addField('!listcat = show all categories', '_ _')
-  .addField('!listcat [category] = show all models within the category', '_ _')
-
-const HelpMenuInfoEmbed = new Discord.RichEmbed()
-  .setColor('777575')
-  .setTitle('!info Command Information')
-  .setAuthor('Printbot', 'https://raw.githubusercontent.com/sparrdrem/printbot/master/_previmg.png')
-  .setThumbnail('https://raw.githubusercontent.com/sparrdrem/printbot/master/_previmg.png')
-  .addBlankField()
-  .addField('Usage: !info [category] [model]', '_ _')
+const fs = require('fs');
+const dirPath = 'categories/';
+var ver = '0.00.03.10'
+var listcat = '!listcat'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  
-  // Variables
-  var listcat = '!listcat'
-
-  // Set up category list
-  const dirPath = 'categories/';
-  const fs = require('fs');
 
   // Lists the version and other information
   if (msg.content === '!ver') {
-    msg.channel.send('Printbot for Discord.');
-    msg.channel.send('Version 0.00.02.00');
-    msg.channel.send('Created by SparrDrem, 2019-30-08 10:19:25:00');
-    msg.channel.send('_ _');
-    msg.channel.send('READY.');
+    // Version information
+    var verMenuEmbed = new Discord.RichEmbed()
+      .setColor('777575')
+      .setTitle('Printbot Version Information')
+      .setAuthor('Printbot', 'https://raw.githubusercontent.com/sparrdrem/printbot/master/_previmg.png')
+      .setThumbnail('https://raw.githubusercontent.com/sparrdrem/printbot/master/_previmg.png')
+      .addField('_ _', '_ _')
+      .addField('Version ' + ver, '_ _')
+      .addField('Created by SparrDrem, 2019-30-08 10:19:25:00', '_ _')
+      .addField('_ _', '_ _')
+      .addField('READY.', '_ _')
+    msg.channel.send(verMenuEmbed);
   }
 
   // Help menu for commands
@@ -62,13 +38,44 @@ client.on('message', msg => {
     // If the 1st argument is listcat...
     if (helpcmd=='listcat') {
       // Send the help menu for listcat.
+      var HelpMenuListcatEmbed = new Discord.RichEmbed()
+        .setColor('777575')
+        .setTitle('!listcat Command Information')
+        .setAuthor('Printbot', 'https://raw.githubusercontent.com/sparrdrem/printbot/master/_previmg.png')
+        .setThumbnail('https://raw.githubusercontent.com/sparrdrem/printbot/master/_previmg.png')
+        .addField('_ _', '_ _')
+        .addField('!listcat = show all categories', '_ _')
+        .addField('!listcat [category] = show all models within the category', '_ _')
+        .addField('_ _', '_ _')
+        .addField('READY.', '_ _')
       msg.channel.send(HelpMenuListcatEmbed);
     // Else, if the 1st argument is info...
     } else if (helpcmd=='info') {
       // Send the help menu for info
+      var HelpMenuInfoEmbed = new Discord.RichEmbed()
+        .setColor('777575')
+        .setTitle('!info Command Information')
+        .setAuthor('Printbot', 'https://raw.githubusercontent.com/sparrdrem/printbot/master/_previmg.png')
+        .setThumbnail('https://raw.githubusercontent.com/sparrdrem/printbot/master/_previmg.png')
+        .addField('_ _', '_ _')
+        .addField('Usage: !info [category] [model]', '_ _')
+        .addField('_ _', '_ _')
+        .addField('READY.', '_ _')
       msg.channel.send(HelpMenuInfoEmbed);
     } else {
     // Else, send the default help menu.
+      var HelpMenuEmbed = new Discord.RichEmbed()
+        .setColor('777575') // Sets border color to a nice gray color
+        .setTitle('Printbot available commands')
+        .setAuthor('Printbot', 'https://raw.githubusercontent.com/sparrdrem/printbot/master/_previmg.png')
+        .setThumbnail('https://raw.githubusercontent.com/sparrdrem/printbot/master/_previmg.png')
+        .addField('Commands:', '_ _')
+        .addField('_ _', '_ _')
+        .addField('!help', 'show this menu')
+        .addField('!ver', 'list current version')
+        .addField('!listcat', 'list all categories \(use `!help listcat` for more detailed view\)')
+        .addField('!info', 'get the link to a 3D model, as well as info about it \(use `!help info` for more detailed view\)')
+        .addField('READY.', '_ _')
       msg.channel.send(HelpMenuEmbed);
     }
   }
@@ -80,42 +87,34 @@ client.on('message', msg => {
     const command = args.shift().toLowerCase();
     // Set the first argument as the category.
     var cat = args[0]
-    // If the first arugment is not blank
-    if (typeof cat != 'undefined') {
-      // AND it exists
-      if (fs.existsSync(dirPath + cat)) {
-        // Set count cariable to keep track of the current number in the list
-        cvar = 0
-        msg.channel.send('3D Models under the "' + cat + '" category:')
-        msg.channel.send('_ _')
-        fs.readdirSync(dirPath + cat).forEach(file => {
-          // Add 1 to count variable then list all files in the subdirectory
-          cvar++
-          msg.channel.send(String(cvar)+'.) '+file);
-        });
-        // Else (aka if the category given does not exist)
-      } else {
-        msg.channel.send('ERROR: ' + cat + ' is not a valid category. Run "!listcat" for a list of categories.')
+    // List all categories
+    function compileList(cat) {
+        console.log(cat);
+        if (fs.existsSync(dirPath + cat)) {
+          var categories = fs.readdirSync(dirPath + cat);
+          } else {
+              var categories = fs.readdirSync(dirPath);
+          }
+        console.log(categories);
+      var cvar = 1;
+      var compiledList = " ";
+      while (cvar<=categories.length) {
+          var compiledList = compiledList.concat(cvar + ".) " + categories[cvar-1] + "\n");
+          cvar++;
       }
+      console.log(compiledList);
+      var categoryList = new Discord.RichEmbed()
+          .setColor('777575')
+          .setTitle('Printbot Categories')
+          .setAuthor('Printbot', 'https://raw.githubusercontent.com/sparrdrem/printbot/master/_previmg.png')
+          .setThumbnail('https://raw.githubusercontent.com/sparrdrem/printbot/master/_previmg.png')
+          .addField(compiledList, '_ _')
+          .addField('_ _', '_ _')
+          .addField('READY.', '_ _')
+      msg.channel.send(categoryList);
     }
-    // If cat is not defined (aka the first argument doesn't exist)
-    if (typeof cat == 'undefined') {
-      // List all categories
-      msg.channel.send('Printbot Categories');
-      msg.channel.send('_ _');
-      // Set a count variable to keep track of what category it is currently on
-      var cvar = 0
-      fs.readdirSync(dirPath).forEach(file => {
-        // Add one to the count variable and list all categories
-        cvar++
-        msg.channel.send(String(cvar)+'.) '+file);
-      });
-      msg.channel.send('_ _');
-      msg.channel.send('This is just beta - there\'s more coming soon!');
-      msg.channel.send('_ _');
+      compileList(cat);
     }
-    msg.channel.send('READY.');
-  }
 
   // Get information on models
   if (msg.content.startsWith('!info')) {
@@ -123,19 +122,31 @@ client.on('message', msg => {
     const args = msg.content.slice('!info'.length).split(' ');
     const command = args.shift().toLowerCase();
     // Set arguments as variables
-    var cat = args[0]
-    var model = args[1]
+    const cat = args[0]
+    const model = args[1]
     // Check if the model exists in the category
     if (fs.existsSync(dirPath + cat + "/" + model)) {
-      // If it does, display the file that has information on the model
-      fs.readFile(dirPath + cat + "/" + model, "utf8", function(err, data) {
-        msg.channel.send(data);
-        msg.channel.send('READY.')
-      });
+      var modelInfo = fs.readFileSync(dirPath + cat + "/" +model, "utf8");
+      var embedModelInfo = new Discord.RichEmbed()
+        .setColor('777575')
+        .setTitle('Model Information')
+        .setAuthor('Printbot', 'https://raw.githubusercontent.com/sparrdrem/printbot/master/_previmg.png')
+        .setThumbnail('https://raw.githubusercontent.com/sparrdrem/printbot/master/_previmg.png')
+        .addField(modelInfo, '_ _')
+        .addField('_ _', '_ _')
+        .addField('READY.', '_ _')
+      msg.channel.send(embedModelInfo);
     } else {
       // If it doesn't, display an error
-      msg.channel.send('ERROR: ' + cat + ' is not a valid category and/or model. Run "!listcat" for a list of categories and to list models within said categories.');
-      msg.channel.send('READY.')
+      var errorFileNotFound = new Discord.RichEmbed()
+        .setColor('777575')
+        .setTitle('ERROR')
+        .setAuthor('Printbot', 'https://raw.githubusercontent.com/sparrdrem/printbot/master/_previmg.png')
+        .setThumbnail('https://raw.githubusercontent.com/sparrdrem/printbot/master/_previmg.png')
+        .addField('Error: That 3D Model or category was not found.', '_ _')
+        .addField('_ _', '_ _')
+        .addField('READY.', '_ _')
+      msg.channel.send(errorFileNotFound);
     }
   }
 
