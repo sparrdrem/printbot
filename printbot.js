570d71896df700284ec460214caaebd6482f8cad
Innovation Inc <connorrox60@gmail.com>
2019-09-12 23:45:53 -0500

    Update printbot.js

--- printbot.js
+++ printbot.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
 const dirPath = 'categories/';
-var ver = '0.00.03.15'
+var ver = '0.00.03.16'
 var listcat = '!listcat'
 
 client.on('ready', () => {
